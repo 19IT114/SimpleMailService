cee67686158135b0b5e62d26215d2390863185e2
19IT114 <19it114@charusat.edu.in>
2021-10-19 09:02:47 +0530

    Initial commit

---
+++ src/test/java/it/it114/SimpleMailService/SimpleMailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.it114.SimpleMailService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
