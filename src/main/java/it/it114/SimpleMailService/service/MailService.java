8a17a8393d93a7985cf14d26384f66dbacaf6fdb
19IT114 <19it114@charusat.edu.in>
2021-11-13 17:22:43 +0530

    updated functionallity #1

---
+++ src/main/java/it/it114/SimpleMailService/service/MailService.java
@@ -0,0 +1,5 @@
+package it.it114.SimpleMailService.service;
+
+public interface MailService {
+    public boolean sendMail(String name, String email, String message);
+}
---
+++ src/main/java/it/it114/SimpleMailService/service/MailServiceImpl.java
@@ -0,0 +1,39 @@
+package it.it114.SimpleMailService.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
+
+@Service
+public class MailServiceImpl implements MailService {
+
+    @Autowired
+    JavaMailSender mailSender;
+
+
+    @Override
+    public boolean sendMail(String name, String email, String messageFromUser) {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+        String userMessage = "Dear CodersAge, Mr./Ms " + name + " is trying to connect. Following is the message from him/her:  " + messageFromUser;
+
+        try {
+            helper.setFrom(email, name);
+            helper.setTo("19it114@charusat.edu.in");
+            helper.setSubject("[URGENT] --> " + name + " is trying to contact you through your website");
+            helper.setText(userMessage, true);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        mailSender.send(message);
+        return false;
+    }
+}
